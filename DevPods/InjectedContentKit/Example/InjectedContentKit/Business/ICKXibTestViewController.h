5d80e95be407d4b5b16d167c103cd19f94dfde62
aron <862709539@qq.com>
2018-04-08 16:10:47 +0800

    Xib test VC

---
+++ DevPods/InjectedContentKit/Example/InjectedContentKit/Business/ICKXibTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  ICKXibTestViewController.h
+//  InjectedContentKit_Example
+//
+//  Created by aron on 2018/4/8.
+//  Copyright © 2018年 flypigrmvb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICKXibTestViewController : UIViewController
+
+@end
